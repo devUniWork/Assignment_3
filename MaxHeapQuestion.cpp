fbfeb1b468fd771e349c79432053b6cd6546a7b5
Devin Reeks <devinwreeks@gmail.com>
2021-08-15 17:59:54 +1000

    does a breadth first search for max heap and writes to file.

--- MaxHeapQuestion.cpp
+++ MaxHeapQuestion.cpp
@@ -1,29 +1,44 @@
 // student no: A00030840
 #include "MaxHeapQuestion.h";
 #include "MaxHeap.h";
-
+#include <string> 
 void MaxHeapQuestion::start_max_heap(string InputFileName, string OutputFileName) {
 	int numberElements = extractValues.number_elements(InputFileName);
-	int* dataElements = new int[numberElements];
-	int* outputMaxHeap = {};
-	int levels = 0;
+	int *dataElements = new int[numberElements];
+	int *outputMaxHeap = new int[numberElements];
+	int iterations = 0;
+	int pointer = 0;
+
 	MaxHeap maxHeap;
 
-
 	dataElements = extractValues.get_elements(InputFileName, numberElements);
+	// to create file.
+	utility.writeToLastLine(OutputFileName, "");
 
-	levels = height(numberElements);
 
 	for (int i = 0; i < numberElements; i++) {
 		maxHeap.insert(dataElements[i]);
+		if (i >= 1) {
+			for (int c = 0; c <= i; c++) {
+				// data elements before insertion.
+				utility.writeToLastLine(OutputFileName, std::to_string(dataElements[iterations]) + " ");
+				iterations++;
+			}
+			iterations = 0;
+			utility.writeToLastLine(OutputFileName, "\n");
+			for (int x = 0; x <= i; x++) {
+				// elements inserted.
+				utility.writeToLastLine(OutputFileName, std::to_string(maxHeap.heap[pointer]) + " ");
+				pointer++;
+			}
+			pointer = 0;
+			utility.writeToLastLine(OutputFileName, "\n");
+		}
+		else {	
+			utility.writeToLastLine(OutputFileName, std::to_string(dataElements[i]) + " ");
+			utility.writeToLastLine(OutputFileName, "\n");
+			utility.writeToLastLine(OutputFileName, std::to_string(maxHeap.heap[i]) + " ");
+			utility.writeToLastLine(OutputFileName, "\n");
+		}
 	}
-
-	for (int z = 0; z < numberElements; z++) {
-		cout << maxHeap.heap[z] << " ";
-	}
-}
-
-int MaxHeapQuestion::height(int N)
-{
-	return ceil(log2(N + 1)) - 1;
 }
--- MaxHeapQuestion.h
+++ MaxHeapQuestion.h
@@ -2,16 +2,16 @@
 #define MAX_HEAP_QUESTION_H
 #include <iostream>
 #include "ExtractValuesFile.h";
-
+#include "utility.h";
 using namespace std;
 
 class MaxHeapQuestion
 {
 private:
 	ExtractValuesFile extractValues;
+	Utility utility;
 public:
 	void start_max_heap(string InputFileName, string OutputFileName);
-	int height(int numberOfNodes);
 };
 #endif
 
